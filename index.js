016c02495c70a8b598e8111c12829335f2aa8ca3
Guang <jouyang19@proton.me>
2024-03-30 17:51:01 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const catAppend = [...cats.slice(), name];
+    return catAppend;
+}
+
+function prependCat(name) {
+    const catPrepend = [name, ...cats.slice()];
+    return catPrepend;
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
